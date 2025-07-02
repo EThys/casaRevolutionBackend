e343dfafc466bb0c761fa19753010e55ee898da1
ElieOko <elieoko100@gmail.com>
2025-07-02 13:40:34 +0100

    feat:new api bailleur and typecard

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\PropertyVisitController;
 use App\Http\Controllers\PropertyFeatureController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BailleurController;
 use App\Http\Controllers\PropertyTypeController;
-
+use App\Http\Controllers\TypeCardController;
 
 //ROUTES POUR L'AUTHENTIFICATION
 Route::post('register', [AuthController::class, 'register']);
@@ -19,7 +20,7 @@ Route::post('reset-password', [AuthController::class, 'resetPassword']);
 Route::get('properties', [PropertyController::class, 'index']);
 Route::get('properties/{id}', [PropertyController::class, 'show']);
 
- //ROUTES POUR LES FONCTIONNALITES
+//ROUTES POUR LES FONCTIONNALITES
 Route::get('features', [PropertyFeatureController::class, 'index']);
 Route::get('features/{feature}', [PropertyFeatureController::class, 'show']);
 Route::get('features/{feature}/properties', [PropertyFeatureController::class, 'properties']);
@@ -29,6 +30,10 @@ Route::get('property', [PropertyTypeController::class, 'index']);
 Route::get('property/{propertyType}', [PropertyTypeController::class, 'show']);
 Route::get('propertyType/{propertyTypeId}/properties', [PropertyTypeController::class, 'getPropertiesByType']);
 
+//ROUTES POUR BAILLEUR parrainage
+Route::resource('bailleur', BailleurController::class)->only(['index', 'store']);
+//ROUTES POUR TypeCard
+Route::resource('type-card', TypeCardController::class)->only(['index']);
 Route::middleware('auth:sanctum')->group(function () {
     //ROUTES POUR L'AUTHENTIFICATION
     Route::get('profile', [AuthController::class, 'profile']);
@@ -50,19 +55,11 @@ Route::middleware('auth:sanctum')->group(function () {
     //ROUTES POUR LES RESERVATIONS
     Route::get('visits/status/{status}', [PropertyVisitController::class, 'getByStatus']);
     Route::put('visits/{id}/status', [PropertyVisitController::class, 'changeStatus']);
-    Route::post('visit',[PropertyVisitController::class, 'store']);
+    Route::post('visit', [PropertyVisitController::class, 'store']);
 
     //ROUTES POUR LES PROPERTYTYPE
 
     Route::post('propertyType', [PropertyTypeController::class, 'store']);
     Route::put('propertyType/{propertyType}', [PropertyTypeController::class, 'update']);
     Route::delete('propertyType/{propertyType}', [PropertyTypeController::class, 'destroy']);
-
-
 });
-
-
-
-
-
-

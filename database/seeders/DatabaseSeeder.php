fac8fb95648caf5874008014f2eb28c1b8a449d0
EThys <ethbergmuzola500@gmail.com>
2025-09-03 16:43:09 +0100

    Refactor: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            PropertyFeatureSeeder::class,
+            PropertyTypeSeeder::class
+        ]);
+
 
         DB::table('TTypeCards')->insert(
             [
